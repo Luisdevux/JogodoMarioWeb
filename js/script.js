d244e871aa8215412d472b3f3a27111cc04688db
Luis Felipe <luis.felipe.lopes1275@gmail.com>
2025-04-04 21:54:36 -0400

    Criação do Código JavaScript da Animação Jump

---
+++ js/script.js
@@ -0,0 +1,11 @@
+const mario = document.querySelector('.mario');
+const pipe = document.querySelector('.pipe');
+const clouds = document.querySelector('.clouds');
+
+const jump = () => {
+    mario.classList.add('jump');
+
+    setTimeout(() => {
+        mario.classList.remove('jump');
+    }, 500);
+}
